e2623df1b280565a6f0144bcb4556c376a4c8f8c
LeoQ <leoq91@gmail.com>
2017-02-21 14:21:00 -0800

    Initial set up with firebase

---
+++ app/scripts/Controllers/LandingCtrl.js
@@ -0,0 +1,9 @@
+ (function() {
+     function LandingCtrl() {
+     	this.heroTitle = "To do to do to do list!!";
+     }
+ 
+     angular
+         .module('blocitoff')
+         .controller('LandingCtrl', LandingCtrl);
+ })();
